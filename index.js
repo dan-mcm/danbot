0d67e7a8887d7c4d22127dc0b2e17e75bfd2675f
daniel40392 <daniel40392@gmail.com>
2020-06-09 21:38:03 +0100

    updated welcome image

--- index.js
+++ index.js
@@ -83,10 +83,10 @@ client.on("guildMemberAdd", async member => {
   // channel variable appears unusable... might need hardcoded channel... could set via .env
 	const channel = member.guild.channels.cache.find(channel => channel.name === process.env.WELCOME_CHANNEL);
 	if (!channel) return;
-	const canvas = Canvas.createCanvas(700, 250);
+	const canvas = Canvas.createCanvas(700, 335);
 	const ctx = canvas.getContext('2d');
 
-	const background = await Canvas.loadImage('./images/wallpaper2.jpg');
+	const background = await Canvas.loadImage('./images/sakura.jpeg');
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 	ctx.strokeStyle = '#74037b';
@@ -94,21 +94,21 @@ client.on("guildMemberAdd", async member => {
 
 	// Slightly smaller text placed above the member's display name
 	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#5E1224';
-	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+	ctx.fillStyle = '#000000';
+	ctx.fillText('Welcome to the server,', canvas.width / 1.8, canvas.height / 3.6);
 
 	// Add an exclamation point here and below
 	ctx.font = applyText(canvas, `${member.displayName}!`);
 	ctx.fillStyle = '#000000';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+	ctx.fillText(`${member.displayName}!`, canvas.width / 1.8, canvas.height / 2);
 
 	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+  ctx.arc(237, 147, 90, 0, Math.PI * 2, true);
 	ctx.closePath();
 	ctx.clip();
 
 	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, 25, 25, 200, 200);
+	ctx.drawImage(avatar, 132, 25, 211, 222);
 
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
